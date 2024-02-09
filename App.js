80b0c0373a38318babfd01a9b1313c944bd95a23
SUNITHA123-THUMMA <104547161+SUNITHA123-THUMMA@users.noreply.github.com>
2024-02-09 14:47:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import TextAnalyzer from "./TextAnalyzer";
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <head>
+        <title>Text-Analyzer</title>
+      </head>
+      <TextAnalyzer/>
+    </div>
+  )
+}
+
+export default App
+
